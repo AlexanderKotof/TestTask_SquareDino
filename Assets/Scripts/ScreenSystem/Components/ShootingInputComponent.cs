de249eb526d9915385ff1d7e93ea715c2012da4b
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-16 19:01:21 +0400

    Screen system imported

---
+++ Assets/Scripts/ScreenSystem/Components/ShootingInputComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI.Screens
+{
+    public class ShootingInputComponent : MonoBehaviour, IPointerDownHandler
+    {
+        public event Action<Vector3> Shoot;
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Shoot?.Invoke(eventData.position);
+        }
+    }
+}
